fcf72da58c4d97f4fab2e5f3884f3452fbab5c1e
mrgab <mrgab@192.168.1.6>
2018-06-02 11:27:02 -0400

    Diagrama Melhorado Inicio das classes PacoteIP e Emissor

--- src/Emissor.java
+++ src/Emissor.java
@@ -1,14 +1,22 @@
+
+import java.net.InetAddress;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Emissor;
+
 
 /**
  *
  * @author mrgab
  */
 public class Emissor {
-    
+    public static void main(String[] args){
+        
+    }
+    public static void  enviaPacote(PacoteIP pacote, InetAddress ip, int porta){
+        
+    }
 }
